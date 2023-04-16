165af6d6c7449bd99ea5b7666df9ae360536dc6b
Zak White <zakwht@gmail.com>
2023-04-15 23:15:59 -0700

    improve types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+// Primitive types
+type PType = string | number | boolean;
+// Types accepted by UrlSearchParams constructor
+type USPType = PType | Array<PType>;
+
 const castStr = (s: string): string | boolean | number => {
   if (s === "true") return true;
   if (s === "false") return false;
@@ -5,7 +10,7 @@ const castStr = (s: string): string | boolean | number => {
   return s;
 };
 
-const definedObj = (o: Record<string, any>): Record<string, any> => {
+const definedObj = (o: Record<string, USPType>): Record<string, USPType> => {
   Object.entries(o).forEach(([k, v]) => {
     if (v == null || v === "") delete o[k];
   });
@@ -15,12 +20,13 @@ const definedObj = (o: Record<string, any>): Record<string, any> => {
 export class URLParams {
   private params: URLSearchParams;
 
-  constructor(search?: string | URLSearchParams | Record<string, any>) {
+  constructor(search?: string | URLSearchParams | Record<string, USPType>) {
     if (!search) {
       if (typeof window === "undefined") throw TypeError("No search query");
       this.params = new URLSearchParams(window.location.search);
     } else if (search instanceof URLSearchParams) this.params = search;
     else if (typeof search === "object")
+      //@ts-ignore: URLSearchParams does accept Record<string, USPType>
       this.params = new URLSearchParams(definedObj(search));
     else if (search.includes("?"))
       this.params = new URLSearchParams(
@@ -32,15 +38,10 @@ export class URLParams {
     else this.params = new URLSearchParams(search.split("?")[1] || search);
   }
 
-  append = (
-    key: string,
-    value: string | number | boolean | Array<string | number | boolean>
-  ) => this.params.append(key, value.toString());
+  append = (key: string, value: USPType) =>
+    this.params.append(key, value.toString());
 
-  set = (
-    key: string,
-    value: string | number | boolean | Array<string | number | boolean>
-  ) => this.params.set(key, value.toString());
+  set = (key: string, value: USPType) => this.params.set(key, value.toString());
 
   toURLSearchParams = () => this.params;
 
